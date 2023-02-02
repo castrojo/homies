a585f18c0e74ce1c934c28080b76231d07771480
Jorge O. Castro <jorge.castro@gmail.com>
2023-02-02 16:30:10 -0500

    Update .local/bin/mesa-git.sh

--- private_dot_local/bin/executable_mesa-git.sh
+++ private_dot_local/bin/executable_mesa-git.sh
@@ -1,3 +1,4 @@
 systemctl --user set-environment FLATPAK_GL_DRIVERS=mesa-git
+systemctl --user set-environment RADV_PERFTEST=gpl 
 systemctl --user restart flatpak-portal.service
 RADV_PERFTEST=gpl FLATPAK_GL_DRIVERS=mesa-git flatpak run com.valvesoftware.Steam
